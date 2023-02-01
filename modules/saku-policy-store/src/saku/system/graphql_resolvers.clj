9bae1d3375302afb1b4dfdc62184c89ec0e88555
luchiniatwork <tiago@luchini.nyc>
2023-02-01 17:58:26 -0500

    Add first version of a graphql schema that compiles

--- modules/saku-policy-store/src/saku/system/graphql_resolvers.clj
+++ modules/saku-policy-store/src/saku/system/graphql_resolvers.clj
@@ -7,25 +7,18 @@
 
 (defmethod ig/init-key ::resolvers [_ opts]
   (info {:msg "Initializing Lacinia Resolvers"})
-  {:MutationRoot/upsertResourcePolicies (constantly {})
-   :MutationRoot/upsertIdentityPolicies (constantly {})
-   :MutationRoot/retractPolicies (constantly {})
+  {;;:MutationRoot/upsertResourcePolicies (constantly {})
+   ;;:MutationRoot/upsertIdentityPolicies (constantly {})
+   ;;:MutationRoot/retractPolicies (constantly {})
 
-   :QueryRoot/policies (constantly {})
+   ;;:QueryRoot/policies (constantly {})
    :QueryRoot/identityPolicies (constantly {})
-   :QueryRoot/resourcePolicies (constantly {})
-   :QueryRoot/evaluateOne (constantly {})
-   :QueryRoot/evaluateMany (constantly {})})
+   ;;:QueryRoot/resourcePolicies (constantly {})
+   ;;:QueryRoot/evaluateOne (constantly {})
+   ;;:QueryRoot/evaluateMany (constantly {})
+   })
 
 
-#_(defmethod ig/init-key ::entity-resolvers [_ opts]
-    (info {:msg "Initializing Lacinia Entity Resolvers"})
-    {;; Internal ones
-     :Inspiration (inspirations/get-inspiration-entity opts)
-     :IdeationSession (ideation/get-ideation-session-entity opts)
-     :Tag (tags/get-tags-entities opts)
-
-     ;; External ones
-     :Story (tags/get-story-entities opts)
-     :ServerMetadataGroup (metadata/get-server-metadata-group-external opts)
-     :User (ideation/get-user-entities opts)})
+(defmethod ig/init-key ::entity-resolvers [_ opts]
+  (info {:msg "Initializing Lacinia Entity Resolvers"})
+  {})
