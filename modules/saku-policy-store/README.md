efdc5adb2e5cf07158918c9b89dd28349092085e
luchiniatwork <tiago@luchini.nyc>
2023-02-09 19:02:54 -0500

    Release v23.2.0 for saku policy store

--- modules/saku-policy-store/README.md
+++ modules/saku-policy-store/README.md
@@ -7,5 +7,13 @@ $ docker run -p 8080:8080 \
     -e SAKU_PORT=8080 \
     -e SAKU_DB_URL=xxx \
     -e SAKU_ENV_ID=prod \
-    luchiniatwork/saku-policy-store:v0.0.0
+    luchiniatwork/saku-policy-store:v23.2.0
 ```
+
+
+`SAKU_DB_URL` can be either a volume path where the LLVM DB will be
+saved or a DataLevin URL (more info
+[here](https://github.com/juji-io/datalevin).)
+
+Once running, the API is at `http://localhost:8080/api`. A convenience
+GraphiQL can be found at `http://localhost:8080/ide`
--- modules/saku-policy-store/scripts/bump_version.clj
+++ modules/saku-policy-store/scripts/bump_version.clj
@@ -5,8 +5,14 @@
   (try
     (println (-> (orzo/read-file "resources/version.txt")
                  (orzo/calver "YY.MM.CC")
-                 (orzo/overwrite-file "README.md" #"saku-policy-store:\d+.\d+.\d+"
-                                      #(str "saku-policy-store" %))
+                 (orzo/overwrite-file "README.md" #"saku-policy-store:v\d+.\d+.\d+"
+                                      #(str "saku-policy-store:v" %))
+                 (orzo/overwrite-file "package.json" #"\"version\": \"\d+.\d+.\d+\""
+                                      #(str "\"version\": \"" % "\""))
+                 (orzo/overwrite-file "pom.xml"
+                                      #"<artifactId>saku-policy-store</artifactId>\n(\s*)<version>.+</version>"
+                                      #(str "<artifactId>saku-policy-store</artifactId>\n$1"
+                                            "<version>" % "</version>"))
                  (orzo/save-file "resources/version.txt")
                  (orzo/stage)))
     (System/exit 0)
