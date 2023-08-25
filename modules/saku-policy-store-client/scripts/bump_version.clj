0c9022471290756a633988e0600e95cee90f6879
Kenny Williams <kwill@hey.com>
2023-08-25 12:36:26 -0700

    chore: add files for client version bumping script

--- modules/saku-policy-store-client/scripts/bump_version.clj
+++ modules/saku-policy-store-client/scripts/bump_version.clj
@@ -5,14 +5,8 @@
   (try
     (println (-> (orzo/read-file "version.txt")
                  (orzo/calver "YY.MM.CC")
-                 (orzo/overwrite-file "README.md" #":mvn/version \"\d+.\d+.\d+\""
-                                      #(str ":mvn/version \"" % "\""))
                  (orzo/overwrite-file "package.json" #"\"version\": \"\d+.\d+.\d+\""
                                       #(str "\"version\": \"" % "\""))
-                 (orzo/overwrite-file "pom.xml"
-                                      #"<artifactId>saku-policy-store-client</artifactId>\n(\s*)<version>.+</version>"
-                                      #(str "<artifactId>saku-policy-store-client</artifactId>\n$1"
-                                            "<version>" % "</version>"))
                  (orzo/save-file "version.txt")))
     (System/exit 0)
     (catch Exception e
